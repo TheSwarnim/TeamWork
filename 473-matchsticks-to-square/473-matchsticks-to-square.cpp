0567a10c174460fc6c04ae3e9267b6a191184117
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-07-13 22:29:40 +0530

    Time: 849 ms (21.45%), Space: 10.2 MB (30.36%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -7,6 +7,10 @@ public:
         int sum = accumulate(begin(nums), end(nums), 0);
         if(sum % 4 != 0) return false;
         
+        // realises that if we sort in decreasing then 
+        // we can reduce the runtime because larget sticks
+        // will help in distributing the other sticks 
+        // to take that position that can be a potential pattern
         sort(rbegin(nums), rend(nums));
         
         vector<int> sidesLength(4, 0);
