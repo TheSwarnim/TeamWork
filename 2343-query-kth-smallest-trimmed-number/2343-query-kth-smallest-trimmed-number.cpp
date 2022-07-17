b5d2c85e86bbea632c66414567a371d186ab9d5e
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-07-17 11:27:31 +0530

    Time: 2665 ms (11.11%), Space: 472.7 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-        int n = nums.size(), strsize = nums[0].size();
-        // the approach is to pre calculate early and give answer as per the queries as per the index required
-        unordered_map<int, vector<pair<string, int>>> lis;
+        int n = nums.size(), q = queries.size();
+        vector<pair<string, int>> arr(n);
         
-        vector<int> ans;
-        for(auto &q : queries){
-            int k = q[0], trim = q[1];
-            if(lis.find(trim-1) == lis.end())
-                calcForIthIndex(lis, trim-1, strsize, nums); 
-            ans.push_back(lis[trim-1][k-1].second);
+        for(int i = 0; i < n; i++) {
+            arr[i] = {nums[i], i};
         }
+        
+        vector<int> ans(q);
+        
+        for(int i = 0; i < q; i++) {
+            vector<pair<string, int>> temp = arr;
+            for(pair<string, int>& p : temp) {
+                p.first = p.first.substr(p.first.size() - queries[i][1]);
+            }
+            sort(temp.begin(), temp.end());
+            ans[i] = temp[queries[i][0] - 1].second;
+        }
+        
         return ans;
-    }
-    
-    void calcForIthIndex(unordered_map<int, vector<pair<string, int>>> &lis, int idx, int strsize, vector<string>& nums){
-        for(int j = 0; j < nums.size(); j++){
-            string substr = nums[j].substr(strsize-1-idx);
-            lis[idx].push_back({substr, j});
-        }
-        
-        auto &l = lis[idx];
-        
-        // time to sort the array
-        sort(begin(l), end(l), [&](pair<string, int> &a, pair<string, int> &b){
-            // cout << a.first << " " << b.first << " " << a.second << " " << b.second << endl;
-            string &s1 = a.first, &s2 = b.first;
-            return s1 == s2 ? a.second < b.second : s1.compare(s2) < 0; 
-        });
     }
 };
