a89245a87d292ea42e60c6ce6b3ca14a27451124
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-07-19 00:07:23 +0530

    Time: 1888 ms (9.55%), Space: 155.7 MB (63.53%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
-    int numSubmatrixSumTarget(vector<vector<int>>& A, int target) {
-        int res = 0, m = A.size(), n = A[0].size();
-        for(int i = 0; i < m; i++)
-            for(int j = 1; j < n; j++)
-                A[i][j] += A[i][j-1];
+    /*
+    the question is almost similar to subarray sum with given target
+    to do the same with this question, we create a window of vertical 
+    like belo on a sliding window where we calculate horizontal
+    prefix rowwise(similar to subarray sum) and repeat this in 2d
+          ij
+    k=0 --**---
+    k=1 --**---
+    k=2 --**---
+    k=3 --**---
+    ... -------
+    ... -------
+
+    */
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
         
-        unordered_map<int, int> counter;
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                counter = {{0, 1}};
-                int cur = 0;
-                for(int k = 0; k < m; k++){
-                    cur += A[k][j] - (i > 0 ? A[k][i-1] : 0);
-                    res += counter.find(cur - target) != counter.end() ? counter[cur - target] : 0;
-                    counter[cur]++;
+        for(int i = 0; i < n; i++)
+            for(int j = 1; j < m; j++)
+                matrix[i][j] += matrix[i][j-1];
+        
+        int res = 0;
+        unordered_map<int, int> mp;
+        for(int y1 = 0; y1 < m; y1++)
+            for(int y2 = y1; y2 < m; y2++){
+                mp = {{0, 1}};
+                int sum = 0;
+                for(int x = 0; x < n; x++){
+                    sum += matrix[x][y2] - (y1 > 0 ? matrix[x][y1-1] : 0);
+                    res += mp[sum-target];
+                    mp[sum]++;
                 }
             }
-        }
-        
         return res;
     }
 };
