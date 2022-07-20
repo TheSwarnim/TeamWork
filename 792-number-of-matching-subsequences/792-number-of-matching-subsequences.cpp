1e7ec1ccb1219a48e02b3b96b54eb9707d8a063a
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-07-21 00:38:35 +0530

    Time: 333 ms (58.77%), Space: 48.1 MB (70.32%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:    
     int numMatchingSubseq(string s, vector<string>& words) {
-        int n = s.length(), m = words.size();
+        int n = s.length();
         /*
         1) bruteforce: for each word in words, check if it is a subsequence 
         of s
@@ -18,19 +18,26 @@ public:
         return ans;
         */
         
-        map<char, vector<int>> firstOcc;
+        /*
+        find the idx of all characters of s in a map,
+        now we iterate each word in words, while iterating each character
+        of a word, we maintain a idx which will keep track of the minimum
+        idx of that char in order of increasing order in s
+        */
+        unordered_map<char, vector<int>> occ;
         for(int i = 0; i < n; i++){
-            firstOcc[s[i]].push_back(i);
+            occ[s[i]].push_back(i);
         }
         
         int ans = 0;
         for(auto &w : words){
-            int latest = -1;
+            int idx = -1; // idx that will keep track of idx in s of a possible subsequence w in s
             for(int j = 0; j < w.size(); j++){
                 char c = w[j];
-                auto it = upper_bound(firstOcc[c].begin(), firstOcc[c].end(), latest);
-                if(it == firstOcc[c].end()) break;
-                latest = *it;
+                
+                auto it = upper_bound(begin(occ[c]), end(occ[c]), idx); // find idx of char c in s which have index in s > idx (current index of previous finded i-th character in w)
+                if(it == end(occ[c])) break;
+                idx = *it; // the next idx will be > *it to complete subsequence in s
                 if(j == w.size() - 1) ans++;
             }
         }
