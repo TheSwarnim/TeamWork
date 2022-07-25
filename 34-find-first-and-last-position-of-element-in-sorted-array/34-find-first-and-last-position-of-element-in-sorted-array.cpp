e34c0a2c00a18546a1beb60c1da650b5aec21d24
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-07-25 10:28:59 +0530

    Time: 10 ms (73.39%), Space: 13.7 MB (63.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -6,6 +6,7 @@ public:
     2) second mtd is to write the binary search code for upper bound and lower bound
     */
     vector<int> searchRange(vector<int>& nums, int target) {
+        /*
         auto leftIt = lower_bound(begin(nums), end(nums), target);
         
         if(leftIt == end(nums)) return {-1, -1};
@@ -15,5 +16,48 @@ public:
         auto rightIt = upper_bound(begin(nums), end(nums), target);
         
         return {int(leftIt - begin(nums)), int(rightIt - begin(nums) - 1)};
+        */
+        
+        return {lower_bound(nums, target), upper_bound(nums, target)};
+    }
+    
+    int lower_bound(vector<int> &nums, int target){
+        int i = 0, j = nums.size() - 1;
+        int resIdx = -1;
+        
+        while(i <= j){
+            int m = i + (j-i)/2;
+            
+            if(nums[m] < target)
+                i = m+1;
+            else if(nums[m] > target)
+                j = m-1;
+            else {
+                j = m-1;
+                resIdx = m;
+            }
+        }
+        
+        return resIdx;
+    }
+    
+    int upper_bound(vector<int> &nums, int target){
+        int i = 0, j = nums.size() - 1;
+        int resIdx = -1;
+        
+        while(i <= j){
+            int m = i + (j-i)/2;
+            
+            if(nums[m] < target)
+                i = m+1;
+            else if(nums[m] > target)
+                j = m-1;
+            else {
+                i = m+1;
+                resIdx = m;
+            }
+        }
+        
+        return resIdx;
     }
 };
