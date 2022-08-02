422850dbe948d16b63c22760a8e8d2582b8c1e75
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-08-02 21:25:45 +0530

    Time: 2 ms (74.98%), Space: 57 MB (44.57%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,29 +1,16 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Tuple> pq = new PriorityQueue<>();
-        int m = matrix.length;
-        for(int j =0; j < m; j++) pq.offer(new Tuple(matrix[0][j], 0, j));
-        for(int i = 0; i < k-1; i++){
-            Tuple t = pq.poll();
-            if(t.x == m-1) continue;
-            pq.offer(new Tuple(matrix[t.x+1][t.y], t.x+1, t.y));
+        int n = matrix.length, lo = matrix[0][0], hi = matrix[n-1][n-1] + 1; // [lo, hi)
+        while(lo < hi) {
+            int mid = lo + (hi - lo)/2;
+            int count = 0, j = n-1;
+            for(int i = 0; i < n; i++){
+                while(j >= 0 && matrix[i][j] > mid) j--;
+                count += (j+1);
+            }
+            if(count < k) lo = mid+1;
+            else hi = mid;
         }
-        return pq.poll().val;
-    }
-    
-    class Tuple implements Comparable<Tuple>{
-        int val;
-        int x;
-        int y;
-        Tuple(int val, int x, int y){
-            this.val = val;
-            this.x = x;
-            this.y = y;
-        }
-        
-        @Override
-        public int compareTo(Tuple that){
-            return this.val - that.val;
-        }
+        return lo;
     }
 }
