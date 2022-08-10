6f871d593183b628757c15da28b7efa61146ec5e
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-08-10 22:53:36 +0530

    Time: 10 ms (80.84%), Space: 6.6 MB (77.88%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,36 +1,25 @@
-class Solution
-{
+class Solution {
 public:
-    int countVowelPermutation(int n);
-};
-
-int Solution :: countVowelPermutation(int n)
-{
-    vector<vector<long>> dp(n+1, vector<long>(5, 0));
-    
-    int MOD = 1e9 + 7;
-    
-    /* dp[i][j] denotes the number of valid strings of length i */
-    
-    for(int i = 0; i < 5; i++)
-        dp[1][i] = 1;
-    
-    for(int i = 1; i < n; i++)
-    {
-        dp[i+1][0] = (dp[i][1] + dp[i][2] + dp[i][4]) %MOD;
-        
-        dp[i+1][1] = (dp[i][0] + dp[i][2]) % MOD;
-        
-        dp[i+1][2] = (dp[i][1] + dp[i][3]) % MOD;
-        
-        dp[i+1][3] = dp[i][2];
-        
-        dp[i+1][4] = (dp[i][2] + dp[i][3]) % MOD;
+    int mod = 1e9 + 7;
+    int countVowelPermutation(int n) {
+        long dp[n+1][5]; // n, (a, e, i, o, u)
+        memset(dp, 0, sizeof(dp));
+        for(int i = 1; i <= n; i++){
+            if(i == 1) 
+                for(int j = 0; j < 5; j++)
+                    dp[i][j] = 1;
+            else {
+                dp[i][0] = (dp[i-1][1] + dp[i-1][2] + dp[i-1][4])%mod;
+                dp[i][1] = (dp[i-1][0] + dp[i-1][2])%mod;
+                dp[i][2] = (dp[i-1][1] + dp[i-1][3])%mod;
+                dp[i][3] = (dp[i-1][2])%mod;
+                dp[i][4] = (dp[i-1][2] + dp[i-1][3])%mod;
+            }
+        }
+        long res = 0;
+        for(int j = 0; j < 5; j++){
+            res = (res + dp[n][j])%mod;
+        }
+        return res;
     }
-    
-    int res = 0;
-    for(int i = 0; i < 5; i++)
-        res = (res + dp[n][i]) % MOD;
-    
-    return res;
-}
+};
