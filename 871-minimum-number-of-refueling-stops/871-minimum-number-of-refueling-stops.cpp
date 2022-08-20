1b49c6b8f839047776e98cfce523d331ec80ac89
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-08-20 22:57:01 +0530

    Time: 75 ms (25.44%), Space: 16 MB (93.38%) - LeetHub

---
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& s) {
+        long dp[501] = {startFuel};
+        // dp[i] = maximum distance we can go with ith fueling
+        for(int i = 0; i < s.size(); i++){
+            for(int t = i; t >= 0 && dp[t] >= s[i][0]; t--){
+                dp[t+1] = max(dp[t+1], dp[t] + s[i][1]);
+            }
+        }
+        for(int t = 0; t <= s.size(); t++){
+            if(dp[t] >= target) return t;
+        }
+        return -1;
+    }
+};
