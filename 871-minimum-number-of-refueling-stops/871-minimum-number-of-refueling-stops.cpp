789ae82f581a78890b8ca69a0400b584cf5144be
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-08-21 14:16:25 +0530

    Time: 40 ms (68.31%), Space: 16.2 MB (51.25%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int minRefuelStops(int target, int startFuel, vector<vector<int>>& s) {
-        int n = s.size();
-        /*
-        gas stations comes in increasing order
-        dp[t] max distance I can cover if I fuel t-th times
-        */
-        long dp[501] = {startFuel};
-        
-        for(int i = 0; i < n; i++){
-            for(int t = i; t >= 0 && dp[t] >= s[i][0]; t--){
-                dp[t+1] = max(dp[t+1], s[i][1] + dp[t]);
-            }
+    int minRefuelStops(int target, int cur, vector<vector<int>>& s) {
+        int n = s.size(),res = 0, i = 0;
+        priority_queue<int> pq;
+        while(cur < target){
+            while(i < n && s[i][0] <= cur)
+                pq.push(s[i++][1]);
+            if(pq.empty()) return -1;
+            cur += pq.top(); pq.pop();
+            res++;
         }
-        
-        for(int t = 0; t <= n; t++){
-            if(dp[t] >= target) return t;
-        }
-        return -1;
+        return res;
     }
 };
