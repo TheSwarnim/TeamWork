59812f47b0f2633dfa960fcc5d5d13f023f4fd2c
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-08-21 13:33:41 +0530

    Time: 214 ms (100.00%), Space: 61.7 MB (55.56%) - LeetHub

--- 2386-find-the-k-sum-of-an-array/2386-find-the-k-sum-of-an-array.cpp
+++ 2386-find-the-k-sum-of-an-array/2386-find-the-k-sum-of-an-array.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     using ll = long long;
     long long kSum(vector<int>& nums, int k) {
-        int n = nums.size();
         vector<ll> ans;
         priority_queue<pair<ll, ll>> pq;
-        ll sum = 0;
-        for(int i = 0; i < n; i++){
-            sum += nums[i] > 0 ? nums[i] : 0;
-            nums[i] = abs(nums[i]);
+        ll n = nums.size(), sum = 0;
+        for(int &x : nums){
+            sum += x > 0 ? x : 0;
+            x = abs(x);
         }
-        
         sort(begin(nums), end(nums));
-        pq.push({sum - nums[0], 0});
         ans.push_back(sum);
+        pq.push({sum-nums[0], 0});
         while(ans.size() < k){
-            auto [val, index] = pq.top();
-            pq.pop();
-            ans.push_back(val);
-            if(index + 1 < n){
-                pq.push({val+nums[index] - nums[index+1], index+1});
-                pq.push({val-nums[index+1], index+1});
+            auto [nextSum, i] = pq.top(); pq.pop();
+            ans.push_back(nextSum);
+            if(i+1 < n){
+                pq.push({nextSum - nums[i+1], i+1});
+                pq.push({nextSum - nums[i+1] + nums[i], i+1});
             }
         }
         return ans[k-1];
-   }
+    }
 };
