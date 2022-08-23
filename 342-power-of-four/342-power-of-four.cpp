66e5cc818638387879c45f5fe131308b114f96a2
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-08-23 23:50:00 +0530

    Time: 3 ms (58.67%), Space: 5.9 MB (71.30%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
-    bool isPowerOfFour(int n) {
-        return n > 0 && (n&(n-1)) == 0 && (n-1)%3 == 0;
+    bool isPowerOfFour(int n) 
+    {
+        if(n<=0)
+            return false;
+        if(n==1)
+            return true;
+        
+        return n%4==0 && isPowerOfFour(n/4);
     }
 };
