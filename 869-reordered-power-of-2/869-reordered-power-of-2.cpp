ce34a95db04660a16eb7e9e10e53c9082b22d30f
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-08-28 16:59:57 +0530

    Time: 78 ms (21.63%), Space: 13.9 MB (58.84%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,37 +1,4 @@
-class Solution {
-public:    
-    bool reorderedPowerOf2(int n) {
-        auto cnt = findCnt(n);
-        
-        long long val = 1;
-        while(val <= int(1e9)){
-            if(canMatch(cnt, val)) {
-                return true;
-            } 
-            val *= 2;
-        }
-        
-        return false;
-    }
-    
-    bool canMatch(vector<int> &cnt, long long pow){
-        auto powCnt = findCnt(pow);
-        
-        for(int i = 0; i < 10; i++){
-            if(powCnt[i] != cnt[i]){
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    vector<int> findCnt(long long n){
-        vector<int> cnt(10, 0);
-        while(n > 0){
-            cnt[n%10]++;
-            n/=10;
-        }
-        return cnt;
-    }
-};
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        c = collections.Counter(str(n))
+        return any(c == collections.Counter(str(1<<i)) for i in range(30))
