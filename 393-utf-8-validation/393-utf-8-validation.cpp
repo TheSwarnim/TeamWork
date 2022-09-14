d8942329641a3f5ccb30092274bc2063091269dc
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-09-14 12:24:07 +0530

    Time: 43 ms (9.73%), Space: 14 MB (76.81%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int rbytes = 0;
+        
+        for(int val : data){
+            if(rbytes == 0){
+                if( (val >> 7) == 0b0 ){ // 1st byte of one length char
+                    rbytes = 0;
+                } else if( (val >> 5) == 0b110 ){ // 1st byte of 2 length char
+                    rbytes = 1;
+                } else if( (val >> 4) == 0b1110 ){ // 1st byte of 3 length char
+                    rbytes = 2;
+                } else if( (val >> 3) == 0b11110 ){ // 1st byte of 4 length char
+                    rbytes = 3;
+                } else {
+                    return false;
+                }
+            } else {
+                if( (val >> 6) == 0b10 ){
+                    rbytes--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return rbytes == 0;
+    }
+};
