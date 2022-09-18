de0bb7b5a0ca48df183ff1262468b774c37442e4
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-09-18 12:58:04 +0530

    Time: 3 ms (40.74%), Space: 41.1 MB (67.85%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,10 +1,9 @@
 class Solution {
     public int mySqrt(int x) {
-        long val = 0, res = 0;
-        while(val*val <= x){
-            res = val;
-            val++;            
+        long r = x;
+        while(r*r > x){
+            r = (r + x/r)/2;
         }
-        return (int)(res);
+        return (int)(r);
     }
 }
