59eea219d47e5a0ac9a3616aede45c0628af01d3
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-09-18 21:23:21 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (98.46%) - LeetHub

---
+++ 319-bulb-switcher/319-bulb-switcher.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        // digit dp
+        /*
+        pos = 10
+        tight = 2
+        sum = 10*9 = 90
+        
+        dp[pos][tight][sum]
+        */
+        
+        return (int)Math.sqrt(n);
+    }
+}
