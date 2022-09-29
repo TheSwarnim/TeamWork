339b9ff3fa9557e1d39ad6eaf8163620f0376287
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-09-29 22:56:11 +0530

    Time: 7 ms (73.79%), Space: 60.3 MB (85.15%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,29 +1,18 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int i = 0, j = arr.length - 1;
-        
-        while(i < j && x >= arr[i]){
-            i++;
-        }
-        i--;
-        j = i+1;
-        
-        LinkedList<Integer> res = new LinkedList<>();
-        while(k-- > 0){
-            if(i < 0){
-                res.addLast(arr[j++]);
-            } else if(j >= arr.length){
-                res.addFirst(arr[i--]);
+        int lo = 0, hi = arr.length - 1;
+        while(hi - lo >= k){
+            if(Math.abs(arr[lo] - x) > Math.abs(arr[hi] - x)){
+                lo++;
             } else {
-                // System.out.println(k + " " + arr[i] + " " + arr[j] + " " + x);
-                if((x - arr[i]) <= (arr[j] - x)){
-                    res.addFirst(arr[i--]);
-                } else {
-                    res.addLast(arr[j++]);
-                }
+                hi--;
             }
         }
         
-        return res;
+        List<Integer> result = new ArrayList<>(k);
+		for (int i = lo; i <= hi; i++) {
+			result.add(arr[i]);
+		}
+		return result;
     }
 }
