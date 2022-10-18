d5170067c79252e395f65acd6178aea5b896e53e
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-10-18 10:39:56 +0530

    Time: 84 ms (54.61%), Space: 14 MB (28.81%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,11 +1,11 @@
 class Solution:
-    def countAndSay(self, n: int) -> str:
-        if n == 1: return "1"
-        
-        prevAns = self.countAndSay(n-1)
+    def countAndSay(self, n: int) -> str:       
+        return "1" if n == 1 else self.computeStr(self.countAndSay(n-1))
+    
+    def computeStr(self, prevResult : str) -> str:
         cnt, val = 0, "$"
         res = ""
-        for c in prevAns:
+        for c in prevResult:
             if val != c:
                 if val != "$":
                     res += str(cnt) + val
