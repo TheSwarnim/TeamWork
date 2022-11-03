03ca1fb8d8d687126462f9dd80c884b99a765d04
Swarnim Pratap Singh <54211377+TheSwarnim@users.noreply.github.com>
2022-11-03 10:34:20 +0530

    Time: 812 ms (50.49%), Space: 167.9 MB (60.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        unordered_map<string, int> same, diff;
-        int res = 0, mxsame = 0;
-        string mxstr;
+        unordered_map<string, int> mp;
+        int unpaired = 0, ans = 0;
         for(auto &w : words){
             if(w[0] == w[1]){
-                same[w]++;
-            } else {
-                diff[w]++;
-            }
-        }
-        for(auto &p : same){
-            if(p.second%2 == 1 && mxsame < p.second){
-                mxsame = p.second;
-                mxstr = p.first;
-            }
-        }
-        res += mxsame*2;
-        for(auto &w : words){
-            if(w[0] == w[1]) {
-                if(w != mxstr){
-                    res += 4*(same[w]/2);
-                    same[w] = 0;
+                if(mp[w] > 0){
+                    unpaired--;
+                    mp[w]--;
+                    ans += 4;
+                } else {
+                    unpaired++;
+                    mp[w]++;
                 }
-                
-                continue;
+            } else {
+                string rev = w;
+                reverse(rev.begin(), rev.end());
+                if(mp[rev] > 0){
+                    mp[rev]--;
+                    ans += 4;
+                } else {
+                    mp[w]++;
+                }
             }
-            
-            string opp = w;
-            reverse(opp.begin(), opp.end());
-            // cout << w << " " << opp << endl;
-            int val = min(diff[w], diff[opp]);
-            diff[w] -= val;
-            diff[opp] -= val;
-            res += 4*val;
         }
-        
-        return res;
+        if(unpaired > 0) ans += 2;
+        return ans;
     }
 };
